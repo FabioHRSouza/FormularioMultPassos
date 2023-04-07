de9dd624721c85bae909f647ec5cb1704a5b7725
Fabio Souza <fabiohrsouza@gmail.com>
2023-04-07 11:51:48 -0300

    Iniciando o Projeto - Exercício 01

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+
+function App() {
+  
+  return (
+    <div className="App">
+      <h1>[M1S10] Ex 1 - Criando o projeto e adicionando as dependências</h1>
+
+    </div>
+  )
+}
+
+export default App
